46d609a8878c22665829be423b07cc4faf82b7e2
Siergej Sobolewski <s.sobolewski@hotmail.com>
2025-03-28 14:04:51 -0400

    Create imp_sqlite3.py

---
+++ import/imp_sqlite3.py
@@ -0,0 +1,37 @@
+# -----------------------------------------------------------------------------
+# csv-db-bridge — Universal Import/Export Toolkit for CSV and Databases
+# -----------------------------------------------------------------------------
+# Author: Siergej Sobolewski <s.sobolewski@hotmail.com>
+# License: GNU General Public License v3.0 (GPL-3.0)
+#
+# This file is part of csv-db-bridge.
+# 
+# csv-db-bridge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# csv-db-bridge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
+import sqlite3
+import csv
+
+conn = sqlite3.connect('mydatabase.db')
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM mytable')
+data = cursor.fetchall()
+
+with open('data.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow([description[0] for description in cursor.description])
+    for row in data:
+        csvwriter.writerow(row)
+
+conn.close()
