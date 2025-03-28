19b153eb3019aae897920656c82651fa05a65ff8
Siergej Sobolewski <s.sobolewski@hotmail.com>
2025-03-28 14:15:19 -0400

    Create exp_postgresql.py

---
+++ export/exp_postgresql.py
@@ -0,0 +1,42 @@
+# -----------------------------------------------------------------------------
+# csv-db-bridge — Universal Import/Export Toolkit for CSV and Databases
+# -----------------------------------------------------------------------------
+# Author: Siergej Sobolewski <s.sobolewski@hotmail.com>
+# License: GNU General Public License v3.0 (GPL-3.0)
+#
+# This file is part of csv-db-bridge.
+# 
+# csv-db-bridge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# csv-db-bridge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="mydatabase",
+    user="myuser",
+    password="mypassword"
+)
+cur = conn.cursor()
+cur.execute("SELECT * FROM mytable")
+rows = cur.fetchall()
+
+with open('export_postgresql.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([desc[0] for desc in cur.description])
+    writer.writerows(rows)
+
+cur.close()
+conn.close()
