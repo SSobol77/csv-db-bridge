96c0eaf6e6dcf613865c7b228e5529c9229ff2c1
Siergej Sobolewski <s.sobolewski@hotmail.com>
2025-03-28 14:35:38 -0400

    Create example_sqlite3_import.py

---
+++ examples/example_sqlite3_import.py
@@ -0,0 +1,5 @@
+# Example: Insert data into SQLite3 database from CSV or command line input
+
+from import.imp_sqlite3 import *
+
+# Modify imp_sqlite3.py to match your table structure and database path.
