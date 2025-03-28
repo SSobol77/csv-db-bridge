3fd5e8735e0d9f910a3dc6eabce8faa8de80df28
Siergej Sobolewski <s.sobolewski@hotmail.com>
2025-03-28 14:36:58 -0400

    Create  example_mongodb_import.py

---
+++ examples/example_mongodb_import.py
@@ -0,0 +1,5 @@
+# Example: Insert documents into MongoDB collection from CLI or CSV
+
+from import.imp_mongodb import *
+
+# You can modify imp_mongodb.py to read from any data source and push to MongoDB
