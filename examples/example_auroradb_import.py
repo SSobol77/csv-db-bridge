fda5012e498fdb1308ca84671ece88947f75a23a
Siergej Sobolewski <s.sobolewski@hotmail.com>
2025-03-28 14:37:25 -0400

    Create example_auroradb_import.py

---
+++ examples/example_auroradb_import.py
@@ -0,0 +1,5 @@
+# Example: Insert or query data from AuroraDB using boto3 RDS Data API
+
+from import.imp_auroradb import *
+
+# Set correct AWS resourceArn, secretArn, and database name in imp_auroradb.py
